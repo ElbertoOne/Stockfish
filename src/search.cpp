ef1605244cf61fb78806bd3d0b161aca9aa960df
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-03-24 07:58:02 +0100

    Use > instead of >=

--- src/search.cpp
+++ src/search.cpp
@@ -1024,13 +1024,13 @@ moves_loop: // When in check search starts from here
 
           // Decrease/increase reduction for moves with a good/bad history
           int rHist;
-          if (cmhValue >= 0 && fmhValue >= 0)
+          if (cmhValue > 0 && fmhValue > 0)
           {
-              rHist = (hValue + std::max(cmhValue, fmhValue)) / 16384;
+              rHist = (hValue + std::max(cmhValue, fmhValue)) / 16040;
           }
           else
           {
-              rHist = (hValue + std::min(cmhValue, fmhValue)) / 16384;
+              rHist = (hValue + std::min(cmhValue, fmhValue)) / 16460;
           }
           r = std::max(DEPTH_ZERO, r - rHist * ONE_PLY);
 
