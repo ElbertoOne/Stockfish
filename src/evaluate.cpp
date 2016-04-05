a96c86704800c682a6a7ac52288b6f75bc3a91e5
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-04-05 10:29:01 +0200

    Take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -397,11 +397,11 @@ namespace {
         // number and types of the enemy's attacking pieces, the number of
         // attacked and undefended squares around our king and the quality of
         // the pawn shelter (current 'score' value).
-        attackUnits =  std::min(71, ei.kingAttackersCount[Them] * ei.kingAttackersWeight[Them])
+        attackUnits =  std::min(70, ei.kingAttackersCount[Them] * ei.kingAttackersWeight[Them])
                      +  9 * ei.kingAdjacentZoneAttacksCount[Them]
                      + 26 * popcount<Max15>(undefended)
                      + 12 * !!ei.pinnedPieces[Us]
-                     - 65 * !pos.count<QUEEN>(Them)
+                     - 66 * !pos.count<QUEEN>(Them)
                      - mg_value(score) / 8;
 
         // Analyse the enemy's safe queen contact checks. Firstly, find the
