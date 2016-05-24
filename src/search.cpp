47e3648360b92c5898fc5b30b478f5f50bfdafd3
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-05-24 10:48:15 +0200

    http://tests.stockfishchess.org/tests/view/57414c2f0ebc59301a354fe0

--- src/search.cpp
+++ src/search.cpp
@@ -1024,11 +1024,16 @@ moves_loop: // When in check search starts from here
           int rHist = (val - 10000) / 20000;
           r = std::max(DEPTH_ZERO, r - rHist * ONE_PLY);
 
+          // Decrease reduction for killers
+          if (PvNode && (move == ss->killers[0] || move == ss->killers[1]))
+              r = std::max(DEPTH_ZERO, r - ONE_PLY);
+
           // Decrease reduction for moves that escape a capture. Filter out
           // castling moves, because they are coded as "king captures rook" and
           // hence break make_move(). Also use see() instead of see_sign(),
           // because the destination square is empty.
-          if (   r
+          else if (   r
+              && !(!PvNode && cutNode)
               && type_of(move) == NORMAL
               && type_of(pos.piece_on(to_sq(move))) != PAWN
               && pos.see(make_move(to_sq(move), from_sq(move))) < VALUE_ZERO)
