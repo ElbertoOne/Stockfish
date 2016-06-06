44403712c17b5ccd62c0bd2833c3daf483196122
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-06-06 22:08:16 +0200

    Lower to 3 * ONE_PLY

--- src/search.cpp
+++ src/search.cpp
@@ -1010,7 +1010,7 @@ moves_loop: // When in check search starts from here
 
           // Increase reduction for cut nodes
           if (!PvNode && cutNode)
-              r += 5 * ONE_PLY;
+              r += 3 * ONE_PLY;
 
           // Decrease reduction for moves that escape a capture. Filter out
           // castling moves, because they are coded as "king captures rook" and
