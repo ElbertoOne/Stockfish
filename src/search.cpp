3607670fbba2993d45f7aa468689b21a9938af75
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-06-13 08:55:14 +0200

    Include fix in IID

--- src/search.cpp
+++ src/search.cpp
@@ -855,7 +855,7 @@ namespace {
     {
         Depth d = depth - 2 * ONE_PLY - (PvNode ? DEPTH_ZERO : depth / 4);
         ss->skipEarlyPruning = true;
-        search<NT>(pos, ss, alpha, beta, d, true);
+        search<NT>(pos, ss, alpha, beta, d, !PvNode);
         ss->skipEarlyPruning = false;
 
         tte = TT.probe(posKey, ttHit);
