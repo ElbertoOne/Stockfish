aba00509b01d7fb50695b90d51304801fff41901
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-06-20 09:03:24 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -730,6 +730,14 @@ namespace {
                  &&  ei.pi->pawn_span(strongSide) <= 1
                  && !pos.pawn_passed(~strongSide, pos.square<KING>(~strongSide)))
             sf = ei.pi->pawn_span(strongSide) ? ScaleFactor(51) : ScaleFactor(37);
+        //Drawish when R vs. R and more or less opposite pawn chains
+        else if (   pos.non_pawn_material(WHITE) == RookValueMg
+                 && pos.non_pawn_material(BLACK) == RookValueMg
+                 && pos.count<PAWN>(strongSide) <= pos.count<PAWN>(~strongSide) + 1
+                 && ei.pi->pawn_asymmetry() <= 1
+                 && ei.pi->passed_pawns(WHITE) == 0
+                 && ei.pi->passed_pawns(BLACK) == 0)
+            sf = ScaleFactor(37);
     }
 
     return sf;
