29dc22241f086a81bac2d2f9b6b27d67539bda90
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-09-30 19:17:37 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -185,7 +185,7 @@ namespace {
   };
 
   // Assorted bonuses and penalties used by evaluation
-  const Score MinorBehindPawn     = S(16,  0);
+  const Score MinorBehindPawn[]   = { S(14,  0), S(18,0) };
   const Score BishopPawns         = S( 8, 12);
   const Score RookOnPawn          = S( 8, 24);
   const Score TrappedRook         = S(92,  0);
@@ -306,10 +306,15 @@ namespace {
                    score += ReachableOutpost[Pt == BISHOP][!!(ei.attackedBy[Us][PAWN] & bb)];
             }
 
-            // Bonus when behind a pawn
-            if (    relative_rank(Us, s) < RANK_5
-                && (pos.pieces(PAWN) & (s + pawn_push(Us))))
-                score += MinorBehindPawn;
+            // Bonus when behind a pawn, more when the pawn is protected
+            if (    relative_rank(Us, s) < RANK_5)
+            {
+                Bitboard behindPawnBB = pos.pieces(PAWN) & (s + pawn_push(Us));
+                if (behindPawnBB)
+                {
+                    score += MinorBehindPawn[!!(ei.attackedBy[Us][ALL_PIECES] & behindPawnBB)];
+                }
+            }
 
             // Penalty for pawns on the same color square as the bishop
             if (Pt == BISHOP)
