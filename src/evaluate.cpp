cbb6d830e5ad0da1994781b82fe0b3ecd1575d58
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-10-04 22:32:31 +0200

    Update comment

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -307,7 +307,7 @@ namespace {
                    score += ReachableOutpost[Pt == BISHOP][!!(ei.attackedBy[Us][PAWN] & bb)];
             }
 
-            // Bonus when behind a pawn or another minor
+            // Bonus when behind a pawn
             if (    relative_rank(Us, s) < RANK_5
                 && (pos.pieces(PAWN) & (s + pawn_push(Us))))
                 score += MinorBehindPawn;
