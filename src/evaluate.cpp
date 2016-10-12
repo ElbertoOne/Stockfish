0a3aee5a54c29cc6c71a7769809981f71447cfff
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-10-12 10:18:34 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -560,7 +560,12 @@ namespace {
         while (b)
             score += Threat[Rook ][type_of(pos.piece_on(pop_lsb(&b)))];
 
-        score += Hanging * popcount(weak & ~ei.attackedBy[Them][ALL_PIECES]);
+        b =  weak & (  ~ei.attackedBy[Them][ALL_PIECES]
+                     | (ei.attackedBy[Them][QUEEN]  & ~ei.attackedBy2[Them] & ei.attackedBy2[Us])
+                     | (ei.attackedBy[Them][ROOK]   & ~ei.attackedBy2[Them] & ei.attackedBy2[Us] & (ei.attackedBy[Us][ROOK]   | ei.attackedBy[Us][BISHOP] | ei.attackedBy[Us][KNIGHT] | ei.attackedBy[Us][PAWN]))
+                     | (ei.attackedBy[Them][BISHOP] & ~ei.attackedBy2[Them] & ei.attackedBy2[Us] & (ei.attackedBy[Us][BISHOP] | ei.attackedBy[Us][KNIGHT] | ei.attackedBy[Us][PAWN]))
+                     | (ei.attackedBy[Them][KNIGHT] & ~ei.attackedBy2[Them] & ei.attackedBy2[Us] & (ei.attackedBy[Us][BISHOP] | ei.attackedBy[Us][KNIGHT] | ei.attackedBy[Us][PAWN])));
+        score += Hanging * popcount(b);
 
         b = weak & ei.attackedBy[Us][KING];
         if (b)
