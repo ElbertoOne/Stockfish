d536cbcbc96d079cc8d6c097fb79762ab5b11782
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-11-10 14:03:32 +0100

    Update pawns.cpp

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -43,8 +43,9 @@ namespace {
   // Connected pawn bonus by opposed, phalanx, twice supported and rank
   Score Connected[2][2][2][RANK_NB];
 
-  // Doubled pawn penalty
-  const Score Doubled = S(18,38);
+  // Doubled pawn penalty by supported flag
+  Score Doubled[2] = { S(16, 34), S(8, 17) };
+  TUNE(SetRange(0, 70), Doubled);
 
   // Lever bonus by rank
   const Score Lever[RANK_NB] = {
@@ -168,7 +169,7 @@ namespace {
             score += Connected[opposed][!!phalanx][more_than_one(supported)][relative_rank(Us, s)];
 
         if (doubled)
-            score -= Doubled;
+            score -= Doubled[doubled && supported];
 
         if (lever)
             score += Lever[relative_rank(Us, s)];
