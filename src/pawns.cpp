d95d0eac71dbb6c81b79f0bfc1fc68aa8824570d
ElbertoOne <ElbertoOne@users.noreply.github.com>
2016-11-25 10:44:52 +0100

    Update pawns.cpp

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -113,11 +113,22 @@ namespace {
     e->pawnsOnSquares[Us][BLACK] = popcount(ourPawns & DarkSquares);
     e->pawnsOnSquares[Us][WHITE] = pos.count<PAWN>(Us) - e->pawnsOnSquares[Us][BLACK];
 
-    Bitboard temp = ourPawns << 8;
-    temp |= temp << 8;
-    temp |= temp << 16;
-    temp |= temp << 32;
+    Bitboard temp;
 
+    if (Us == WHITE)
+    {
+        temp = ourPawns << 8;
+        temp |= temp << 8;
+        temp |= temp << 16;
+        temp |= temp << 32;
+    }
+    else
+    {
+        temp = ourPawns >> 8;
+        temp |= temp >> 8;
+        temp |= temp >> 16;
+        temp |= temp >> 32;
+    }
     e->pawnHoles[Us] = ~e->pawnAttacks[Us] & temp;
 
     // Loop through all pawns of the current color and score each pawn
