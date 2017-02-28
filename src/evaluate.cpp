c0f8902bf964032cb94cf2f6885835be576a0d44
ElbertoOne <ElbertoOne@users.noreply.github.com>
2017-02-28 13:49:09 +0100

    Take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -744,11 +744,11 @@ namespace {
     int kingDistance =  distance<File>(pos.square<KING>(WHITE), pos.square<KING>(BLACK))
                       - distance<Rank>(pos.square<KING>(WHITE), pos.square<KING>(BLACK));
     int blockedPawns = 2* popcount(ei.pe->blocked_pawns(WHITE)); //blocked pawns should be equal for black and white
-    int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK);
+    int pawns = pos.count<PAWN>(WHITE) + pos.count<PAWN>(BLACK) - blockedPawns;
     bool bothFlanks = (pos.pieces(PAWN) & QueenSide) && (pos.pieces(PAWN) & KingSide);
 
     // Compute the initiative bonus for the attacking side
-    int initiative = 8 * (asymmetry + kingDistance - blockedPawns - 16) + 12 * pawns + 16 * bothFlanks;
+    int initiative = 8 * (asymmetry + kingDistance - 17) + 15 * pawns + 16 * bothFlanks;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
