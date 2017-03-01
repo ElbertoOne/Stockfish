15f96721ac8c21b1c1ccecf53428aa418a71aa06
ElbertoOne <ElbertoOne@users.noreply.github.com>
2017-03-01 07:00:19 +0100

    Take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -857,8 +857,8 @@ Value Eval::evaluate(const Position& pos) {
 
   int totalMobility = mobility[WHITE] + mobility[BLACK];
   int mobilityFactor = 0;
-  if (nonPawnMaterial > 0)\
-      mobilityFactor = ((totalMobility/nonPawnMaterial) - 3767) / 200;
+  if (nonPawnMaterial > 0)
+      mobilityFactor = ((totalMobility/nonPawnMaterial) - 3767) / 150;
   // Evaluate position potential for the winning side
   score += evaluate_initiative(pos, ei.pe->pawn_asymmetry(), mobilityFactor, eg_value(score));
 
