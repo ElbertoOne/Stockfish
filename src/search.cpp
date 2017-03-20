36eac96c995c2a21be58dd5608ba348382965f7a
ElbertoOne <ElbertoOne@users.noreply.github.com>
2017-03-20 12:40:35 +0100

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -904,7 +904,8 @@ moves_loop: // When in check search starts from here
       {
           if (   !captureOrPromotion
               && !givesCheck
-              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= 5000))
+              && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= 5000)
+              && !pos.pawn_on_7th())
           {
               // Move count based pruning
               if (moveCountPruning) {
@@ -1257,7 +1258,8 @@ moves_loop: // When in check search starts from here
       if (   !InCheck
           && !givesCheck
           &&  futilityBase > -VALUE_KNOWN_WIN
-          && !pos.advanced_pawn_push(move))
+          && !pos.advanced_pawn_push(move)
+          && !pos.pawn_on_7th())
       {
           assert(type_of(move) != ENPASSANT); // Due to !pos.advanced_pawn_push
 
