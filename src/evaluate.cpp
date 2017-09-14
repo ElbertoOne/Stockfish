094da214b53f5e95f1f7afd9c989d9cb5f672f4d
ElbertoOne <lubvdberg@gmail.com>
2017-09-14 16:53:27 +0200

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -493,6 +493,8 @@ namespace {
         // Transform the kingDanger units into a Score, and substract it from the evaluation
         if (kingDanger > 0)
             score -= make_score(kingDanger * kingDanger / 4096, kingDanger / 16);
+        else if (kingDanger < 0)
+            score -= make_score(0, kingDanger / 16);
     }
 
     // King tropism: firstly, find squares that opponent attacks in our king flank
