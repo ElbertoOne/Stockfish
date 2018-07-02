a434032b5a042f54118bea0c3f281aec45f02648
ElbertoOne <lubvdberg@gmail.com>
2018-07-02 09:28:34 +0200

    Take 3

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -388,11 +388,11 @@ namespace {
                 score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
 
             // Penalty when trapped by the king, even more if the king cannot castle
-            else if (mob <= 3 && !(b & mobilityArea[Us] & file_bb(s)))
+            else if (mob <= 3)
             {
                 File kf = file_of(pos.square<KING>(Us));
                 if ((kf < FILE_E) == (file_of(s) < kf))
-                    score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + !pos.can_castle(Us));
+                    score -= (TrappedRook - make_score(mob * 22, 0)) * (1 + (relative_rank(Us, s) == RANK_1) * !pos.can_castle(Us));
             }
         }
 
