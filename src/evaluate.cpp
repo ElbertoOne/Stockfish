0b61ae356cd0ee52a9237d0641b8ee2c58d9eb6b
ElbertoOne <lubvdberg@gmail.com>
2018-08-09 11:18:10 +0200

    Take 2

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -769,7 +769,7 @@ namespace {
                     + 16 * pawnsOnBothFlanks
                     + 48 * !pos.non_pawn_material()
                     + imbalance
-                    -189 ;
+                    -136 ;
 
     // Now apply the bonus: note that we find the attacking side by extracting
     // the sign of the endgame value, and that we carefully cap the bonus so
