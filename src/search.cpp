74472acc9bd0f6eec4a35b756e7cafad56b95880
ElbertoOne <lubvdberg@gmail.com>
2018-09-11 11:12:27 +0200

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -756,7 +756,7 @@ namespace {
     // Step 9. Null move search with verification search (~40 Elo)
     if (   !PvNode
         && (ss-1)->currentMove != MOVE_NULL
-        && (ss-1)->statScore < 23200
+        && (ss-1)->statScore < 23200 - 50 * (depth / ONE_PLY) * (depth / ONE_PLY)
         &&  eval >= beta
         &&  ss->staticEval >= beta - 36 * depth / ONE_PLY + 225
         && !excludedMove
