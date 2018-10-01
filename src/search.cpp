09e7803f2748e0ebbe608464843dd094fe80fb78
ElbertoOne <lubvdberg@gmail.com>
2018-10-01 15:27:19 +0200

    Bench: 4404558

--- src/search.cpp
+++ src/search.cpp
@@ -942,7 +942,7 @@ moves_loop: // When in check, search starts from here
               && (!pos.advanced_pawn_push(move) || pos.non_pawn_material() >= Value(5000)))
           {
               // Move count based pruning (~30 Elo)
-              if (moveCountPruning && pos.non_pawn_material() >= Value(5000))
+              if (moveCountPruning && !skipQuiets)
               {
                   skipQuiets = true;
                   continue;
