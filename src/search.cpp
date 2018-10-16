8e891bc8e68b557fb404a8294647b06c522bac81
ElbertoOne <lubvdberg@gmail.com>
2018-10-16 16:15:37 +0200

    Bench: 4355752

--- src/search.cpp
+++ src/search.cpp
@@ -1011,7 +1011,10 @@ moves_loop: // When in check, search starts from here
           if (!captureOrPromotion)
           {
               // Decrease reduction for exact PV nodes (~0 Elo)
-              if (pvExact || quietCount > 20)
+              if (pvExact)
+                  r -= ONE_PLY;
+
+              if (quietCount > 20)
                   r -= ONE_PLY;
 
               // Increase reduction if ttMove is a capture (~0 Elo)
