8d26da5a87be7e13673b31b003d42298781ff506
ElbertoOne <lubvdberg@gmail.com>
2018-11-09 11:00:16 +0100

    Bench: 3762132

--- src/search.cpp
+++ src/search.cpp
@@ -85,10 +85,10 @@ namespace {
     return d > 17 ? 0 : 29 * d * d + 138 * d - 134;
   }
 
-  // Add a small random component to draw evaluations to keep search dynamic 
+  // Add a small random component to draw evaluations to keep search dynamic
   // and to avoid 3fold-blindness.
   Value value_draw(Depth depth, Thread* thisThread) {
-    return depth < 4 ? VALUE_DRAW 
+    return depth < 4 ? VALUE_DRAW
                      : VALUE_DRAW + Value(2 * (thisThread->nodes.load(std::memory_order_relaxed) % 2) - 1);
   }
 
@@ -598,7 +598,7 @@ namespace {
         if (   Threads.stop.load(std::memory_order_relaxed)
             || pos.is_draw(ss->ply)
             || ss->ply >= MAX_PLY)
-            return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos) 
+            return (ss->ply >= MAX_PLY && !inCheck) ? evaluate(pos)
                                                     : value_draw(depth, pos.this_thread());
 
         // Step 3. Mate distance pruning. Even if we mate at the next move our score
@@ -946,7 +946,7 @@ moves_loop: // When in check, search starts from here
       // Extension for king moves that change castling rights
       if (   pos.can_castle(us)
           && type_of(movedPiece) == KING
-          && depth < 12 * ONE_PLY)
+          && depth < 8 * ONE_PLY)
           extension = ONE_PLY;
 
       // Calculate new depth for this move
