f8acac118fa0cd973519df12003f1bba7e94af8a
ElbertoOne <lubvdberg@gmail.com>
2018-11-28 13:43:41 +0100

    Bench: 3463789

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -550,7 +550,7 @@ namespace {
                 score += ThreatByRank * (int)relative_rank(Them, s);
         }
 
-        if (weak & attackedBy[Us][KING])
+        if (weak & attackedBy[Us][KING] & ~(attackedBy[Them][ALL_PIECES] & ~attackedBy2[Us]))
             score += ThreatByKing;
 
         score += Hanging * popcount(weak & ~attackedBy[Them][ALL_PIECES]);
