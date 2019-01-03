98251b13d2d544763e6118e38258011bd0a0517c
ElbertoOne <lubvdberg@gmail.com>
2019-01-03 11:50:41 +0100

    Bench: 3515386

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -464,11 +464,15 @@ namespace {
     // the square is in the attacker's mobility area.
     unsafeChecks &= mobilityArea[Them];
 
+    int pieceDifference =  popcount(kingFlank & (pos.pieces(Them) ^ pos.pieces(Them, PAWN, KING)))
+                         - popcount(kingFlank & (pos.pieces(Us)   ^ pos.pieces(Us,   PAWN, KING)));
+
     kingDanger +=        kingAttackersCount[Them] * kingAttackersWeight[Them]
                  +  69 * kingAttacksCount[Them]
                  + 185 * popcount(kingRing[Us] & weak)
                  + 150 * popcount(pos.blockers_for_king(Us) | unsafeChecks)
                  +       tropism * tropism / 4
+                 +   8 * pieceDifference
                  - 873 * !pos.count<QUEEN>(Them)
                  -   6 * mg_value(score) / 8
                  +       mg_value(mobility[Them] - mobility[Us])
