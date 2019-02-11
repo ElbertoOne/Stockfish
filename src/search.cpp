d12f2a082660a70067061e583451c2e46445a290
ElbertoOne <lubvdberg@gmail.com>
2019-02-11 10:41:03 +0100

    Bench: 3746588

--- src/search.cpp
+++ src/search.cpp
@@ -1057,7 +1057,7 @@ moves_loop: // When in check, search starts from here
                   && pos.non_pawn_material(us) > RookValueMg + 2 * KnightValueMg
                   && std::abs(file_of(to_sq(move)) - file_of(pos.square<KING>(~us))) <= 1
                   && std::abs(rank_of(to_sq(move)) - rank_of(pos.square<KING>(~us))) <= 3)
-                  r -= ONE_PLY;
+                  r -= 2 * ONE_PLY;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
