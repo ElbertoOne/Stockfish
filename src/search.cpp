bd686ed5a8c3fbee703156b7523dcbdcc3bb8932
ElbertoOne <lubvdberg@gmail.com>
2019-03-02 22:13:17 +0100

    Bench: 3198898

--- src/search.cpp
+++ src/search.cpp
@@ -1029,7 +1029,7 @@ moves_loop: // When in check, search starts from here
               r -= ONE_PLY;
 
           // Decrease reduction if opponent's move count is high (~10 Elo)
-          if ((ss-1)->moveCount > 15)
+          if ((ss-1)->moveCount > 15 && !pos.capture_or_promotion((ss-1)->currentMove))
               r -= ONE_PLY;
 
           if (!captureOrPromotion)
