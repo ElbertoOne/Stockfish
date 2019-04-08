f9d104d6cc6d1301dc022e994136ed3e378c5405
ElbertoOne <lubvdberg@gmail.com>
2019-04-08 10:21:55 +0200

    Bench: 3368060

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -618,24 +618,21 @@ namespace {
 
     b = pe->passed_pawns(Us);
 
-    // Add additional candidate passed pawns
-    Bitboard kq = pos.pieces(Us, PAWN) & ~b & ~pe->pawn_attacks_span(Them) & ~attackedBy[Them][ALL_PIECES];
-    if (kq)
-    {
-        // Squares strongly protected by the enemy, either because they defend the
-        // square with a pawn, or because they defend the square twice and we don't.
-        Bitboard stronglyProtected =  attackedBy[Them][PAWN]
-                       | (attackedBy2[Them] & ~attackedBy2[Us]);
+    // Squares strongly protected by the enemy, either because they defend the
+    // square with a pawn, or because they defend the square twice and we don't.
+    Bitboard stronglyProtected =  attackedBy[Them][PAWN]
+                   | (attackedBy2[Them] & ~attackedBy2[Us]);
 
-        // Enemy pawns not strongly protected and under our attack
-        Bitboard weak = pos.pieces(Them, PAWN) & ~stronglyProtected & attackedBy[Us][ALL_PIECES];
+    // Enemy pawns not strongly protected and under our attack
+    Bitboard weak = pos.pieces(Them, PAWN) & ~stronglyProtected & attackedBy[Us][ALL_PIECES];
+    if (weak)
+    {
+        // Add additional candidate passed pawns
+        Bitboard kq = pos.pieces(Us, PAWN) & ~b & ~pe->pawn_attacks_span(Them) & ~attackedBy[Them][ALL_PIECES];
         while (kq)
         {
             Square s = pop_lsb(&kq);
-            Square blockSq = s + Up;
-            if (weak & blockSq)
-                b |= s;
-            else if (weak & (blockSq + Up))
+            if (weak & forward_file_bb(Us, s))
                 b |= s;
         }
     }
