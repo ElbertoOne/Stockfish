bb7c0a4858b3ec095d6d3929380ec67efd937f87
ElbertoOne <lubvdberg@gmail.com>
2019-04-12 11:23:48 +0200

    Bench: 3147644

--- src/pawns.cpp
+++ src/pawns.cpp
@@ -136,12 +136,12 @@ namespace {
         }
         else if (backward || !neighbours)
         {
-			if (backward)
+            if (backward)
                 score -= Backward, e->weakUnopposed[Us] += !opposed;
 
             if (!neighbours)
                 score -= Isolated, e->weakUnopposed[Us] += !opposed;
-		}
+        }
 
         if (doubled && !support)
             score -= Doubled;
