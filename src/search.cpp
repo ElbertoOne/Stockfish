aa8ad91c61f682040b62c352cdcf0a111e2b05c9
ElbertoOne <lubvdberg@gmail.com>
2019-05-06 13:38:29 +0200

    Bench: 3272689

--- src/search.cpp
+++ src/search.cpp
@@ -1022,22 +1022,22 @@ moves_loop: // When in check, search starts from here
           if ((ss-1)->moveCount > 15)
               r -= ONE_PLY;
 
+          // Increase reduction for cut nodes (~5 Elo)
+          if (cutNode)
+              r += 2 * ONE_PLY;
+
+          // Decrease reduction for moves that escape a capture. Filter out
+          // castling moves, because they are coded as "king captures rook" and
+          // hence break make_move(). (~5 Elo)
+          else if (    type_of(move) == NORMAL
+                   && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
+              r -= 2 * ONE_PLY;
+
           if (!captureOrPromotion)
           {
               // Increase reduction if ttMove is a capture (~0 Elo)
               if (ttCapture)
                   r += ONE_PLY;
-
-              // Increase reduction for cut nodes (~5 Elo)
-              if (cutNode)
-                  r += 2 * ONE_PLY;
-
-              // Decrease reduction for moves that escape a capture. Filter out
-              // castling moves, because they are coded as "king captures rook" and
-              // hence break make_move(). (~5 Elo)
-              else if (    type_of(move) == NORMAL
-                       && !pos.see_ge(make_move(to_sq(move), from_sq(move))))
-                  r -= 2 * ONE_PLY;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
