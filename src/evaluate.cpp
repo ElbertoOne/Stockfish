300b957e1de67e09fc4f8eece982acaf94af8dba
ElbertoOne <lubvdberg@gmail.com>
2019-05-24 19:02:31 +0200

    Bench: 3684823

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -418,7 +418,11 @@ namespace {
     if (rookChecks)
         kingDanger += RookSafeCheck;
     else
+    {
         unsafeChecks |= b1 & attackedBy[Them][ROOK];
+        if (unsafeChecks & pos.pieces(Us, ROOK, QUEEN))
+            kingDanger += RookSafeCheck / 4;
+    }
 
     // Enemy queen safe checks: we count them only if they are from squares from
     // which we can't give a rook check, because rook checks are more valuable.
