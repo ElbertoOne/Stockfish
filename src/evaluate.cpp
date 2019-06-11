9b9884e39878b2598b5fed7ac00738e5e858240e
ElbertoOne <lubvdberg@gmail.com>
2019-06-11 13:30:23 +0200

    Bench: 3778634

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -152,6 +152,7 @@ namespace {
   constexpr Score ThreatBySafePawn   = S(173, 94);
   constexpr Score TrappedRook        = S( 47,  4);
   constexpr Score WeakQueen          = S( 49, 15);
+  constexpr Score QueenFlanksReach   = S( 20,  0);
 
 #undef S
 
@@ -370,6 +371,9 @@ namespace {
             Bitboard queenPinners;
             if (pos.slider_blockers(pos.pieces(Them, ROOK, BISHOP), s, queenPinners))
                 score -= WeakQueen;
+
+            if ((b & QueenSide) && (b & KingSide))
+                score += QueenFlanksReach;
         }
     }
     if (T)
