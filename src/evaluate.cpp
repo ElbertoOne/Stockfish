c29e7b7a4b02b0688d99102677a6d86a36034e51
ElbertoOne <lubvdberg@gmail.com>
2019-06-12 15:08:12 +0200

    Bench: 3603406

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -143,7 +143,7 @@ namespace {
   constexpr Score MinorBehindPawn    = S( 18,  3);
   constexpr Score Outpost            = S( 36, 12);
   constexpr Score PawnlessFlank      = S( 17, 95);
-  constexpr Score RestrictedPiece    = S(  7,  7);
+  constexpr Score RestrictedPiece    = S(  8,  8);
   constexpr Score RookOnPawn         = S( 10, 32);
   constexpr Score SliderOnQueen      = S( 59, 18);
   constexpr Score ThreatByKing       = S( 24, 89);
