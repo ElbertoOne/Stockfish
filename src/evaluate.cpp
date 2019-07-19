3ff3db77bb64b07329dc9eb51fb970176c292c89
ElbertoOne <lubvdberg@gmail.com>
2019-07-19 08:43:27 +0200

    Bench: 3240839

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -660,7 +660,7 @@ namespace {
                 Bitboard targets = PawnAttacks[Us][s] & pe->passed_pawns(Them);
                 if (   targets
                     && pos.pawn_passed(Us, lsb(targets)))
-                    k += k;
+                    k += k + 5;
 
                 bonus += make_score(k * w, k * w);
             }
