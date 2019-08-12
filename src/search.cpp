b8fc572195b1534ba67cf1e782e7480427f86787
ElbertoOne <lubvdberg@gmail.com>
2019-08-12 08:54:08 +0200

    Bench: 4460993

--- src/search.cpp
+++ src/search.cpp
@@ -855,7 +855,7 @@ namespace {
         int probCutCount = 0;
 
         while (  (move = mp.next_move()) != MOVE_NONE
-               && probCutCount < 2 + 2 * (cutNode && !ttCapture))
+               && probCutCount < 2 + 2 * cutNode - ttCapture)
             if (move != excludedMove && pos.legal(move))
             {
                 probCutCount++;
