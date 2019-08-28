f0bc58b980e8ff23d4b257bf1434cf810a1e6328
ElbertoOne <lubvdberg@gmail.com>
2019-08-28 12:16:03 +0200

    Bench: 3765162

--- src/search.cpp
+++ src/search.cpp
@@ -1099,7 +1099,7 @@ moves_loop: // When in check, search starts from here
           // Decrease reduction if move has been singularly extended
           r -= singularLMR * ONE_PLY;
 
-          if (PvNode && thisThread->best_move_count(move) > 10)
+          if (PvNode && thisThread->best_move_count(move) > 8)
               r += ONE_PLY;
 
           if (!captureOrPromotion)
