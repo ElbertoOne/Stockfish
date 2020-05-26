d8b246de1875d9d1fc83be8c320e9590ef913887
ElbertoOne <lubvdberg@gmail.com>
2020-05-26 11:13:36 +0200

    Bench: 5035886

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -126,6 +126,12 @@ namespace {
     S(0, 0), S(10, 28), S(17, 33), S(15, 41), S(62, 72), S(168, 177), S(276, 260)
   };
 
+ // RookOnKingRing contains a bonus according to the rank number of the most forward pawn
+ // on it's file (0 = no pawn) if the rook aligns with the enemy king ring.
+  constexpr Score RookOnKingRing[] = {
+    S( 22, 0), S( 20, 0), S( 18, 0), S( 12, 0), S( 18, 0), S( 19, 0), S( 10, 0)
+  };
+
   // Assorted bonuses and penalties
   constexpr Score BishopPawns         = S(  3,  7);
   constexpr Score BishopXRayPawns     = S(  4,  5);
@@ -143,7 +149,6 @@ namespace {
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
   constexpr Score RestrictedPiece     = S(  7,  7);
-  constexpr Score RookOnKingRing      = S( 16,  0);
   constexpr Score RookOnQueenFile     = S(  5,  9);
   constexpr Score SliderOnQueen       = S( 59, 18);
   constexpr Score ThreatByKing        = S( 24, 89);
@@ -289,7 +294,13 @@ namespace {
             kingAttacksCount[Us] += popcount(b & attackedBy[Them][KING]);
         }
         else if (Pt == ROOK && (file_bb(s) & kingRing[Them]))
-            score += RookOnKingRing;
+        {
+            Bitboard bp = file_bb(s) & pos.pieces(Us, PAWN);
+            if (bp)
+                score += RookOnKingRing[relative_rank(Us, frontmost_sq(Us, bp))];
+            else
+                score += RookOnKingRing[0];
+        }
 
         int mob = popcount(b & mobilityArea[Us]);
 
