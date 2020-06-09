195fbca075911210c0269dd871f3ecea4edd4a34
ElbertoOne <lubvdberg@gmail.com>
2020-06-09 08:29:43 +0200

    Bench: 4400736

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -330,7 +330,7 @@ namespace {
                 Bitboard blocked = pos.pieces(Us, PAWN) & shift<Down>(pos.pieces());
 
                 score -= BishopPawns * pos.pawns_on_same_color_squares(Us, s)
-                                     * (!(attackedBy[Us][PAWN] & s) + popcount(blocked & CenterFiles)) / (1 + kingRingAttack);
+                                     * (!(attackedBy[Us][PAWN] & s) + !kingRingAttack * popcount(blocked & CenterFiles));
 
                 // Penalty for all enemy pawns x-rayed
                 score -= BishopXRayPawns * popcount(attacks_bb<BISHOP>(s) & pos.pieces(Them, PAWN));
