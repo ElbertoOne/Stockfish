200d52ecf1987eab815d2fa76441ccb1336bac8c
ElbertoOne <lubvdberg@gmail.com>
2020-06-09 15:02:00 +0200

    Bench: 4615805

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -453,8 +453,8 @@ namespace {
     // Find the squares that opponent attacks in our king flank, the squares
     // which they attack twice in that flank, and the squares that we defend.
     b1 = attackedBy[Them][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp;
-    b2 = b1 & attackedBy2[Them];
     b3 = attackedBy[Us][ALL_PIECES] & KingFlank[file_of(ksq)] & Camp;
+    b2 = b1 & (attackedBy2[Them] | ~b3);
 
     int kingFlankAttack = popcount(b1) + popcount(b2);
     int kingFlankDefense = popcount(b3);
