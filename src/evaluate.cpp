785b8c030f1361f4e3fb82fd4cd01eee37214071
ElbertoOne <lubvdberg@gmail.com>
2020-06-10 09:22:40 +0200

    Bench: 4584344

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -465,6 +465,7 @@ namespace {
                  +  69 * kingAttacksCount[Them]
                  +   3 * kingFlankAttack * kingFlankAttack / 8
                  +       mg_value(mobility[Them] - mobility[Us])
+                 + 100 * popcount(pe->passed_pawns(Them) & ~KingFlank[file_of(ksq) & Camp])
                  - 873 * !pos.count<QUEEN>(Them)
                  - 100 * bool(attackedBy[Us][KNIGHT] & attackedBy[Us][KING])
                  -   6 * mg_value(score) / 8
