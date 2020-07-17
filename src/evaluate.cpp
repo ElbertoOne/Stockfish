4046818ac145aa2404254f00565326a0b154903a
ElbertoOne <lubvdberg@gmail.com>
2020-07-17 09:04:59 +0200

    Bench: 4650660

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -314,8 +314,8 @@ namespace {
             if (   Pt == KNIGHT
                 && bb & s & ~CenterFiles
                 && !(b & pos.pieces(Them) & ~pos.pieces(PAWN))
-                && !conditional_more_than_two(
-                      pos.pieces(Them) & ~pos.pieces(PAWN) & (s & QueenSide ? QueenSide : KingSide)))
+                && !more_than_one(
+                      pos.pieces(Them) & ~pos.pieces(PAWN, KING) & (s & QueenSide ? QueenSide : KingSide) & ~(Us == WHITE ? Rank1BB : Rank8BB)))
                 score += BadOutpost;
             else if (bb & s)
                 score += Outpost[Pt == BISHOP];
